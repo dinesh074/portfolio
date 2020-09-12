ce54eeea6bc56ee4583b45ae0248f4ec788a55fd
dinesh <dmanikanta.indian1@gmail.com>
2020-09-12 12:12:28 +0530

    Updates

---
+++ precache-manifest.d7d856ba1ebccfb5f4bf2b7b11ccbea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac583d00f94d4a5411225eec3cac0c5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1c323c81c991dde39f15",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9dbeb74c489db37042eb",
+    "url": "/portfolio/static/css/main.a3b58552.chunk.css"
+  },
+  {
+    "revision": "1c323c81c991dde39f15",
+    "url": "/portfolio/static/js/2.ee7dfe90.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.ee7dfe90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dbeb74c489db37042eb",
+    "url": "/portfolio/static/js/main.42c1b4db.chunk.js"
+  },
+  {
+    "revision": "e446bcb8ed77c9106314",
+    "url": "/portfolio/static/js/runtime-main.17aaeeef.js"
+  }
+]);
